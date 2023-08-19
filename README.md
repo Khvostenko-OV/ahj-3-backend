5ee115fc0900f43dbcc619a5090411d253aa20d7
Oleg Khvostenko <xboctehko@yandex.ru>
2023-08-19 21:05:48 +0200

    Server complete

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-![CI](https://github.com/Khvostenko-OV/ahj-3-backend/actions/workflows/web.yml/badge.svg)
 
 # My http server
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const http = require('http');
+const querystring = require('querystring');
 const Koa = require('koa');
+const koaBody = require('koa-body');
 const app = new Koa();
+const port = process.env.PORT || 7070;
 
 const tickets = [];
+const descriptions = [];
+let ticketId = 0;
 
 // => CORS
 app.use(async (ctx, next) => {
@@ -37,34 +42,81 @@ app.use(async (ctx, next) => {
   }
 });
 
-app.use(async ctx => {
-    console.log(ctx.request.querystring)
-    const { method } = ctx.request.querystring;
+// => Body Parsers
+app.use(koaBody({
+   text: true,
+   urlencoded: true,
+   multipart: true,
+   json: true,
+}));
 
-    switch (method) {
+app.use(async ctx => {
+    const qs = querystring.parse(ctx.request.querystring);
+    const { name, description, status } = ctx.request.body;
+    console.log(qs.method, ctx.request.body)
+
+    switch (qs.method) {
         case 'allTickets':
-            ctx.response.body = tickets;
-            return;
-        // TODO: обработка остальных методов
-        default:
+          ctx.response.body = tickets;
+          return;
+
+        case 'ticketById':
+          if (qs.id && tickets[qs.id]) {
+            const ticket = tickets[qs.id];
+            ticket.description = descriptions[qs.id];
+            ctx.response.body = ticket;
+          } else {
             ctx.response.status = 404;
-            return;
+          }
+          return;
+
+        case 'createTicket':
+          if (name) {
+            tickets[ticketId] = {
+              'id': ticketId,
+              'name': name,
+              'status': Boolean(status),
+              'created': new Date()
+            }
+            descriptions[ticketId] = description || '';
+            ctx.response.body = tickets[ticketId++]
+            ctx.response.status = 201;
+          } else {
+            ctx.response.status = 400;
+          }
+          return;
+
+        case 'patchTicket':
+          if (qs.id && tickets[qs.id]) {
+            tickets[qs.id].name = name || tickets[qs.id].name;
+            if (status !== undefined) {
+              tickets[qs.id].status = (status == 'false') ? false : true; 
+            }
+            if (description !== undefined) {
+              descriptions[qs.id] = description;
+            }
+            ctx.response.body = tickets[qs.id];
+            ctx.response.status = 200;
+          } else {
+            ctx.response.status = 404;
+          }
+          return;
+
+        case 'deleteTicket':
+          if (qs.id && tickets[qs.id]) {
+            tickets[qs.id] = undefined;
+            descriptions[qs.id] = undefined;
+            ctx.response.body = qs.id
+            ctx.response.status = 200;
+          } else {
+            ctx.response.status = 404;
+          }
+          return;
+
+        default:
+          ctx.response.status = 404;
+          return;
     }
 });
 
-const port = process.env.PORT || 7070;
 http.createServer(app.callback()).listen(port)
-
-//const server = http.createServer((req, res) => {
-//  console.log(req.headers);
-//  res.end('server response');
-//});
-
-//const port = 7070;
-// слушаем определённый порт
-//server.listen(port, (err) => {
-//    if (err) {
-//      return console.log('Error occured:', error)
-//    }
-//    console.log(`server is listening on ${port}`)
-//});
